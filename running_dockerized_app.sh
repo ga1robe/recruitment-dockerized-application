7c4f6d03b366bd794a9e317adfad12232a9a1f24
ga1robe <78224732+ga1robe@users.noreply.github.com>
2022-01-04 22:40:36 +0100

    Update file
    Add variable

--- running_dockerized_app.sh
+++ running_dockerized_app.sh
@@ -1,5 +1,6 @@
 
 #!/bin/bash
-cd /opt/test/docker
+docke_dir="/opt/test/docker"
+cd ${docke_dir}
 0 5 * * * root docker-compose --project-name test --file docker-compose-test.yaml up --detach > /dev/null 2>&1
 cd -
