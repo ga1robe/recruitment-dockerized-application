a336f3195021e36ca7ab9e836948cacc79d18da4
ga1robe <78224732+ga1robe@users.noreply.github.com>
2022-01-04 22:42:34 +0100

    update file

--- stopping_dockerized_app.sh
+++ stopping_dockerized_app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docke_dir="/opt/test/docker"
 cd ${docke_dir}
-0 19 * * * root docker-compose --project-name test --file /opt/test/docker/docker-compose-test.yaml down > /dev/null 2>&1
+0 19 * * * root docker-compose --project-name test --file docker-compose-test.yaml down > /dev/null 2>&1
 cd -
