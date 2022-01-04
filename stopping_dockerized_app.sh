6b362a0416552cb1c443ca4be3bf0e480290c92e
ga1robe <78224732+ga1robe@users.noreply.github.com>
2022-01-04 22:41:24 +0100

    update file
    Add variable

--- stopping_dockerized_app.sh
+++ stopping_dockerized_app.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd /opt/test/docker
+docke_dir="/opt/test/docker"
+cd ${docke_dir}
 0 19 * * * root docker-compose --project-name test --file /opt/test/docker/docker-compose-test.yaml down > /dev/null 2>&1
 cd -
